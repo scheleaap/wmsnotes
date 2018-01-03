0f2135ae79e1a3b87882e423f935b90c531b31b7
Wout <git@wout.maaskant.info>
2018-01-03 10:27:04 +0100

    fix: Set window title at startup.

--- src/main/ui/window.py
+++ src/main/ui/window.py
@@ -9,6 +9,8 @@ from application.controller import NoteOpened
 
 class MainWindowHandler(object):
     """Connects and controls a Gtk.Window.
+
+    - Window title
     """
 
     def __init__(
@@ -21,13 +23,18 @@ class MainWindowHandler(object):
 
         bus.subscribe(application.event.APPLICATION_TOPIC, self.on_application_event)
 
+        self.set_title(None)
+
     def on_application_event(self, bus, event):
         self.log.debug(u'Event received: {event}'.format(event=event))
 
         if isinstance(event, NoteOpened):  # type: NoteOpened
-            if event.node is not None:
-                self.window.set_title('WMS Notes - {title}'.format(title=event.node.title))
-            else:
-                self.window.set_title('WMS Notes')
+            self.set_title(event.node)
         else:
             self.log.debug(u'Unhandled event: {event}'.format(event=event))
+
+    def set_title(self, node):
+        if node is not None:
+            self.window.set_title('WMS Notes - {title}'.format(title=node.title))
+        else:
+            self.window.set_title('WMS Notes')

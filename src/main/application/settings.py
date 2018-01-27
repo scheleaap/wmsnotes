3fc937a6c98a919ae03635b032d23584daf8fbff
Wout <git@wout.maaskant.info>
2018-01-27 19:45:48 +0100

    feat: Store window state configuration.

--- src/main/application/settings.py
+++ src/main/application/settings.py
@@ -2,10 +2,25 @@ import configparser
 import enum
 import logging
 import os.path
+from collections import namedtuple
 
 import cyrusbus
 
 from application.event import APPLICATION_TOPIC
+
+__all__ = [
+    'LayoutRequestedEvent'
+    'Settings',
+    'SettingsController',
+    'SettingsRepository'
+]
+
+EDITOR_VIEWER_SPLIT_POSITION_KEY = 'editor_viewer_split_position'
+MAIN_SPLIT_POSITION_KEY = 'main_split_position'
+WINDOW_STATE_KEY = 'window_state'
+WINDOW_SIZE_AND_LOCATION_KEY = 'window_size_and_location'
+
+WindowSizeAndLocation = namedtuple('WindowSizeAndLocation', ['width', 'height', 'left', 'top'])
 
 
 @enum.unique
@@ -15,15 +30,33 @@ class WindowState(enum.Enum):
 
 
 class LayoutRequestedEvent:
-    def __init__(self, window_state: WindowState, main_split_position: int, editor_viewer_split_position: int):
+    def __init__(
+            self,
+            window_size_and_location: WindowSizeAndLocation,
+            window_state: WindowState,
+            main_split_position: int,
+            editor_viewer_split_position: int
+    ):
         self.main_split_position = main_split_position
         self.editor_viewer_split_position = editor_viewer_split_position
+        self.window_size_and_location = window_size_and_location
         self.window_state = window_state
 
 
 class Settings(LayoutRequestedEvent):  # Refactor inheritance if needed
-    def __init__(self, window_state: WindowState, main_split_position: int, editor_viewer_split_position: int):
-        super().__init__(window_state, main_split_position, editor_viewer_split_position)
+    def __init__(
+            self,
+            window_size_and_location: WindowSizeAndLocation,
+            window_state: WindowState,
+            main_split_position: int,
+            editor_viewer_split_position: int
+    ):
+        super().__init__(
+            window_size_and_location,
+            window_state,
+            main_split_position,
+            editor_viewer_split_position
+        )
 
 
 class SettingsRepository:
@@ -47,24 +80,60 @@ class SettingsRepository:
         # It is possible to pass a list of files to config.read().
         config.read([self.config_file_path], encoding='utf-8')
         settings = Settings(
-            window_state=self._get_enum(config, 'DEFAULT', 'window_state', WindowState, WindowState.NORMAL),
-            main_split_position=config.getint('DEFAULT', 'main_split_position', fallback=300),
-            editor_viewer_split_position=config.getint('DEFAULT', 'editor_viewer_split_position', fallback=400),
+            window_size_and_location=self._read_and_parse_window_size_and_location(
+                config, 'DEFAULT', WINDOW_SIZE_AND_LOCATION_KEY, fallback=None),
+            window_state=self._read_and_parse_enum(
+                config, 'DEFAULT', WINDOW_STATE_KEY, WindowState, WindowState.NORMAL),
+            main_split_position=config.getint(
+                'DEFAULT', MAIN_SPLIT_POSITION_KEY, fallback=300),
+            editor_viewer_split_position=config.getint(
+                'DEFAULT', EDITOR_VIEWER_SPLIT_POSITION_KEY, fallback=400),
         )
         return settings
 
-    def _get_enum(self, config, section, key, enum_class, default):
+    def _read_and_parse_enum(self, config, section, key, enum_class, default):
         try:
-            return enum_class(config[section].get(key, default.value))
+            return enum_class[config[section].get(key, default.value)]
         except ValueError:
             return default
+
+    def _read_and_parse_window_size_and_location(self, config, section, key, fallback) -> WindowSizeAndLocation:
+        raw_value = config[section].get(key, None)
+        if raw_value is None:
+            return None
+        try:
+            int_values = [int(v.strip()) for v in raw_value.split(',')]
+        except ValueError:
+            return None
+        if len(int_values) != 4:
+            return None
+        return WindowSizeAndLocation(*int_values)
 
     def save(self, settings: Settings):
         self.ensure_directory_exists()
         config = configparser.ConfigParser()
-        config['DEFAULT'] = settings
+        config['DEFAULT'] = {}
+        if settings.window_size_and_location is not None:
+            config['DEFAULT'][WINDOW_SIZE_AND_LOCATION_KEY] = self._serialize_window_size_and_location(
+                settings.window_size_and_location)
+        if settings.window_state is not None:
+            config['DEFAULT'][WINDOW_STATE_KEY] = settings.window_state.name
+        if settings.main_split_position is not None:
+            config['DEFAULT'][MAIN_SPLIT_POSITION_KEY] = str(settings.main_split_position)
+        if settings.editor_viewer_split_position is not None:
+            config['DEFAULT'][EDITOR_VIEWER_SPLIT_POSITION_KEY] = str(settings.editor_viewer_split_position)
         with open(self.config_file_path, 'w', encoding='utf-8') as f:
             config.write(f)
+
+        def _serialize_window_size_and_location(self, window_size_and_location: WindowSizeAndLocation) -> str:
+            if window_size_and_location is None:
+                return None
+            return ','.join([
+                window_size_and_location.width,
+                window_size_and_location.height,
+                window_size_and_location.left,
+                window_size_and_location.top
+            ])
 
 
 class SettingsController:
@@ -76,13 +145,11 @@ class SettingsController:
         settings = self.repository.load()
         self.publish_event(settings)
 
-    def save_layout(self):
-        """Saves the window layout."""
-        self.repository.save(Settings(
-            window_state=WindowState.MAXIMIZED,
-            main_split_position=0,
-            editor_viewer_split_position=0
-        ))
+    # TODO: Rename to "update" once settings are not flushed to disk immediately anymore
+    def update_and_save(self, f):
+        settings = self.repository.load()
+        f(settings)
+        self.repository.save(settings)
 
     def publish_event(self, event):
         if event is not None:
--- src/main/ui/layout.py
+++ src/main/ui/layout.py
@@ -1,7 +1,7 @@
 import logging
 
 import cyrusbus
-from gi.repository import Gtk
+from gi.repository import Gdk, Gtk
 
 import application.event
 from application.settings import Settings, SettingsController, LayoutRequestedEvent, WindowState
@@ -28,24 +28,36 @@ class LayoutHandler(object):
         self.editor_viewer_split_pane = editor_viewer_split_pane
         self.window = window
 
-        self._on_position_changed_handler_id_main = self.main_split_pane.connect('notify::position',
-                                                                                 self.on_position_changed_main)
-        self._on_position_changed_handler_id_editor_viewer = self.editor_viewer_split_pane.connect('notify::position',
-                                                                                                   self.on_position_changed_editor_viewer)
+        self._on_window_state_changed_handler_id = self.window.connect(
+            'window-state-event', self.on_window_state_changed)
+        self._on_position_changed_handler_id_main = self.main_split_pane.connect(
+            'notify::position', self.on_position_changed_main)
+        self._on_position_changed_handler_id_editor_viewer = self.editor_viewer_split_pane.connect(
+            'notify::position', self.on_position_changed_editor_viewer)
         bus.subscribe(application.event.APPLICATION_TOPIC, self.on_event)
 
     def on_event(self, bus, event):
         self.log.debug(u'Event received: {event}'.format(event=event))
 
         if isinstance(event, LayoutRequestedEvent):  # type: LayoutRequestedEvent
-            with self.main_split_pane.handler_block(self._on_position_changed_handler_id_main):
-                with self.editor_viewer_split_pane.handler_block(self._on_position_changed_handler_id_editor_viewer):
-                    if event.window_state == WindowState.MAXIMIZED:
-                        self.window.maximize()
-                    else:
-                        self.window.unmaximize()
-                        self.window.deiconify()
+
+            with self.main_split_pane.handler_block(self._on_position_changed_handler_id_main), \
+                 self.editor_viewer_split_pane.handler_block(self._on_position_changed_handler_id_editor_viewer), \
+                 self.window.handler_block(self._on_window_state_changed_handler_id):
+
+                if event.window_state == WindowState.MAXIMIZED:
+                    self.window.maximize()
+                elif event.window_state == WindowState.NORMAL:
+                    self.window.unmaximize()
+                    self.window.deiconify()
+
+                if event.window_size_and_location is not None:
+                    self.window.move(event.window_size_and_location.left, event.window_size_and_location.top)
+                    self.window.resize(event.window_size_and_location.width,
+                                       event.window_size_and_location.height)
+                if event.main_split_position is not None:
                     self.main_split_pane.set_position(event.main_split_position)
+                if event.editor_viewer_split_position is not None:
                     self.editor_viewer_split_pane.set_position(event.editor_viewer_split_position)
         else:
             self.log.debug(u'Unhandled event: {event}'.format(event=event))
@@ -65,3 +77,14 @@ class LayoutHandler(object):
             settings.main_split_position = position
 
         self.settings_controller.update_and_save(f)
+
+    def on_window_state_changed(self, _, event: Gdk.EventWindowState):
+        print(event.changed_mask & Gdk.WindowState.MAXIMIZED)
+        if event.changed_mask & Gdk.WindowState.MAXIMIZED:
+            def f(settings: Settings):
+                if event.new_window_state & Gdk.WindowState.MAXIMIZED:
+                    settings.window_state = WindowState.MAXIMIZED
+                else:
+                    settings.window_state = WindowState.NORMAL
+
+            self.settings_controller.update_and_save(f)
